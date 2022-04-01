0fdc3c5e1d3b984c1496cea8adfec29a4954a13d
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2022-04-01 21:32:13 +0200

    Save the inventory

--- sources/initialize.c
+++ sources/initialize.c
@@ -5,6 +5,10 @@
 #include "../headers/characters.h"
 #include "../headers/events.h"
 
+#define NBR_LINES 103
+    /* 103 because: "current_location", id of current_location, "inventory", 100 potential inventory items */
+#define NBR_CHARACTERS_IN_LINE 56
+
 void exit_file_corrupted(FILE* save_file)
 {
     printf("\n\t[Error: The save file has been corrupted.]\n");
@@ -19,7 +23,7 @@ void initialize_game(FILE* save_file)
     int i, j;
     long id;
     char *end_ptr = NULL;
-    char save_buffer[4][56] = {0};
+    char save_buffer[NBR_LINES][NBR_CHARACTERS_IN_LINE] = {0};
     populate_list_lexicon();
     populate_list_locations();
     populate_list_items();
@@ -28,15 +32,23 @@ void initialize_game(FILE* save_file)
 	
     if (save_file != NULL)
     {
-        for (i = 0; i < 4; ++i)
+        for (i = 0; i < NBR_LINES; ++i)
         {
-            /* 55 characters and not 56 because of the null ending character */
-            for (j = 0; j < 55; ++j)
+            /* NBR_CHARACTERS_IN_LINE - 1 because of the null ending character */
+            for (j = 0; j < (NBR_CHARACTERS_IN_LINE - 1); ++j)
             {
                 save_buffer[i][j] = fgetc(save_file);
+
+                /* end of the current line */
                 if (save_buffer[i][j] == '\n')
                 {
                     save_buffer[i][j] = '\0';
+                    break;
+                }
+                /* end of file */
+                else if  (save_buffer[i][j] == EOF)
+                {
+                    i = NBR_LINES;
                     break;
                 }
             }
@@ -58,9 +70,20 @@ void initialize_game(FILE* save_file)
                 PLAYER->current_location = list_locations + id;
         }
 
-        if (save_buffer[2] == NULL || strcmp(save_buffer[2], "debug_value"))
+        if (save_buffer[2] == NULL || strcmp(save_buffer[2], "inventory"))
         {
             exit_file_corrupted(save_file);
+        }
+        else
+        {
+            for (i = 0; i < NBR_ITEMS; ++i)
+            {
+                id = strtol(save_buffer[3 + i], &end_ptr, 10);
+                if (save_buffer[3 + i] == end_ptr || *end_ptr != '\0' || (id < 1 || id > (NBR_ITEMS - 1)))
+                    break;
+                else
+                    PLAYER->list_of_items_by_id[i] = id;
+            }
         }
     }
     return;
--- sources/save.c
+++ sources/save.c
@@ -2,11 +2,18 @@
 
 void save_game(FILE* save_file)
 {
+    int i;
+
     fprintf(save_file, "current_location\n");
     fprintf(save_file, "%d\n", PLAYER->current_location->location_id);
 
-    fprintf(save_file, "debug_value\n");
-    fprintf(save_file, "%d\n", 42);
+    fprintf(save_file, "inventory\n");
+    for (i = 0; i < NBR_ITEMS; ++i)
+    {
+        if (PLAYER->list_of_items_by_id[i] == ID_ITEM_NONE)
+            break;
+        fprintf(save_file, "%d\n", PLAYER->list_of_items_by_id[i]);
+    }
 
     return;
 }
